409de3beb6424a07f0d04132d271d1236da4f46a
Yanislav Igonin <jan.oasis@gmail.com>
2019-11-19 16:19:44 +0300

    Update use pam ssh script

--- ssh/disable_use_pam.sh
+++ ssh/disable_use_pam.sh
@@ -1,3 +1,3 @@
-sed -n 'H;${x;s/\#UsePAM yes/UsePAM no/;p;}' /etc/ssh/sshd_config > /tmp_sshd_config
+sed -n 'H;${x;s/\UsePAM yes/UsePAM no/;p;}' /etc/ssh/sshd_config > /tmp_sshd_config
 cat /tmp_sshd_config > /etc/ssh/sshd_config
 rm /tmp_sshd_config 
