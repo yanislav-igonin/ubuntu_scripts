55f31166576ef0d044091acfd969a00ed439b600
Yanislav Igonin <jan.oasis@gmail.com>
2019-11-19 14:07:51 +0300

    Update docker script

--- docker/docker_ubuntu_18_04.sh
+++ docker/docker_ubuntu_18_04.sh
@@ -4,6 +4,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce
+apt install -y docker-ce
 systemctl enable docker
 docker --version
