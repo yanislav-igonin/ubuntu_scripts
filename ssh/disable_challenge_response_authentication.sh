a88eed5187242027b0bfaf26b857c6936b6b9ff5
Yanislav Igonin <jan.oasis@gmail.com>
2019-11-20 21:02:52 +0300

    Remove some ssh scripts

--- ssh/disable_challenge_response_authentication.sh
+++
@@ -1,3 +0,0 @@
-sed -n 'H;${x;s/\#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/;p;}' /etc/ssh/sshd_config > /tmp_sshd_config
-cat /tmp_sshd_config > /etc/ssh/sshd_config
-rm /tmp_sshd_config
--- ssh/disable_use_pam.sh
+++
@@ -1,3 +0,0 @@
-sed -n 'H;${x;s/\UsePAM yes/UsePAM no/;p;}' /etc/ssh/sshd_config > /tmp_sshd_config
-cat /tmp_sshd_config > /etc/ssh/sshd_config
-rm /tmp_sshd_config 
